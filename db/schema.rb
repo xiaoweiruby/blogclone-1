fa530393bdb6b889c82550590e019189d4a33499
xiaowei <shenzhoudance@gmail.com>
2018-11-09 16:15:57 +0800

    add_comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_09_080610) do
+ActiveRecord::Schema.define(version: 2018_11_09_081205) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
